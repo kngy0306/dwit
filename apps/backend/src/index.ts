eea14d91be3451729137f302ddd85559aa0706cd
kona <kitami.kitano20@gmail.com>
2024-03-17 13:28:51 +0900

    rootのtsconfigを修正

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,7 +2,8 @@ import { createServer } from './server';
 
 const port = process.env.PORT || 5001;
 const server = createServer();
+const name = 'kona';
 
 server.listen(port, () => {
-  console.log(`api running on ${port}`);
+  console.log(`api running on ${port}: from ${name}`);
 });
