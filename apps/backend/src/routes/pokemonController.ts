ae0c5412a049bc87a06f008cb1b859194247230b
kona <kitami.kitano20@gmail.com>
2024-05-11 08:42:17 +0900

    :art: Add pikachu data.

--- apps/backend/src/routes/pokemonController.ts
+++ apps/backend/src/routes/pokemonController.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import { pikachu } from '../assets/api/pokemon/pikachu';
 
 const router: Router = Router();
 
 router.get('/pikachu', (_, res) => {
-  return res.json({ name: 'pikachu' });
+  return res.json(pikachu);
 });
 
 export default router;
